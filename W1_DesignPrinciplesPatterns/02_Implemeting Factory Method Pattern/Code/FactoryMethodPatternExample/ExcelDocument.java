dccfd03ffcb3f686061be4d033f9dd2474079825
Samiullah <msam53077@gmail.com>
2025-06-20 11:29:47 +0530

    Comleted

---
+++ W1_DesignPrinciplesPatterns/02_Implemeting Factory Method Pattern/Code/FactoryMethodPatternExample/ExcelDocument.java
@@ -0,0 +1,15 @@
+
+public class ExcelDocument implements Document{
+    public void creating(){
+        System.out.println("Creating a ExcelDocument");
+    }
+    public void opening(){
+        System.out.println("Opeing a ExcelDocument");
+    }
+    public void saveing(){
+        System.out.println("Saving a ExcelDocument");
+    }
+    public void closing(){
+        System.out.println("Closing a ExcelDocument");
+    }
+}
