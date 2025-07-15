ba8c3e70501344efad6898c7f8edba29b6f83cd3
Samiullah <msam53077@gmail.com>
2025-07-16 00:10:21 +0530

    week 2 hands on

---
+++ Week2_Nunit and MoQ/2_Write Testable Code with Moq/Code/Lib/CustomerComm.cs
@@ -0,0 +1,22 @@
+﻿namespace CustomerCommLib
+{
+    public class CustomerComm
+    {
+        private readonly IMailSender _mailSender;
+
+        public CustomerComm(IMailSender mailSender)
+        {
+            _mailSender = mailSender;
+        }
+
+        public bool SendMailToCustomer()
+        {
+            string customerEmail = "cust123@abc.com";
+            string message = "Some Message";
+
+            _mailSender.SendMail(customerEmail, message);
+
+            return true;
+        }
+    }
+}
